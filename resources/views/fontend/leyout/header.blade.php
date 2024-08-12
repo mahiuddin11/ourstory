37eb68f523f47ae5ae2a3e39f148f5879c9ab525
mahiuddin11 <mahiuddinsamad@gmail.com>
2024-08-12 15:03:28 +0600

    our blog fast up

---
+++ resources/views/fontend/leyout/header.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <title>Mini Blog</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link href="{{ asset('cssfontend') }}../../css.css?family=Muli:300,400,700|Playfair+Display:400,700,900"
+        rel="stylesheet">
+
+    <link rel="stylesheet" href="{{ asset('cssfontend') }}/fonts/icomoon/style.css">
+    <link rel="stylesheet" href="{{ asset('cssfontend') }}/css/bootstrap.min.css">
+    <link rel="stylesheet" href="{{ asset('cssfontend') }}/css/magnific-popup.css">
+    <link rel="stylesheet" href="{{ asset('cssfontend') }}/css/jquery-ui.css">
+    <link rel="stylesheet" href="{{ asset('cssfontend') }}/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="{{ asset('cssfontend') }}/css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="{{ asset('cssfontend') }}/css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="{{ asset('cssfontend') }}/fonts/flaticon/font/flaticon.css">
+    <link rel="stylesheet" href="{{ asset('cssfontend') }}/css/aos.css">
+
+    <link rel="stylesheet" href="{{ asset('cssfontend') }}/css/style.css">
+</head>
+
+<body>
+
+    <div class="site-wrap">
+        <!--side content wrapper -->
